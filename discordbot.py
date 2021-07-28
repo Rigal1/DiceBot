a4bbf7202b71d06b2770e7c109a5068b28924377
Rigal1 <76402544+Rigal1@users.noreply.github.com>
2021-07-29 05:07:18 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -10,15 +10,43 @@ commentOutWord = "(#)"
 
 bot = commands.Bot(command_prefix='/')
 token = os.environ['DISCORD_BOT_TOKEN']
+textDataLink = "textData.txt"
 percent = 0.3
 memberList = []
-emotionTable = ["共感/不信", "友情/怒り", "愛情/妬み", "忠誠/侮蔑", "憧憬/劣等感", "狂信/殺意"]
-botSentences = ["コマンド間違えてるニャ！　気を付けるニャ！",
-                "コマンドが違うニャ！",
-                "また間違えてるのニャ！",
-                "それは違うニャ！",
-                "ニャー！　間違ってるのニャ！",
-                "ほら、間違えてるのニャ"]
+emotionTable = []
+botSentences = []
+help_text = ""
+
+def initialData():
+    getData = readFiles(textDataLink) 
+    global emotionTable
+    global botSentences
+    global helpText
+    #global helpCatText
+    getEmotion = detectData("emotionTable", getData)
+    emotionTable = getEmotion.split(",")
+    getSentence = detectData("errorText", getData)
+    botSentences = getSentence.split(",")
+    helpText = detectData("help", getData)
+    #helpCatText = detectData("help_cat", getData)
+    print(emotionTable)
+
+def readFiles(link):
+    with open(link, encoding = "UTF-8") as f:
+        strData = f.read()
+        listData = strData.split("\n\n")
+        return listData
+    
+def detectData(name, getList):
+    for item in getList:
+        List = item.split(":", 1)
+        #print(List)
+        if len(List) >= 2:
+            if List[0] == name:
+                return List[1]
+            
+    return None
+
 
 
 @bot.event
@@ -50,10 +78,6 @@ async def neko(ctx):
     await ctx.send("ニャーア♪")
     
 @bot.command()
-async def kitaiti(ctx):
-    await ctx.send(f"{ctx.author.mention}**2D6** => 4(2+2) => **4**")
-    
-@bot.command()
 async def ote(ctx):
     if random.random() >= percent:
         await ctx.send(f"{ctx.author.mention} ニャ（ぽふ）")
@@ -66,20 +90,12 @@ async def okawari(ctx):
         await ctx.send(f"{ctx.author.mention} ニャ（ぺふ）")
     else:
         await ctx.send(f"{ctx.author.mention} ニャ（ぷい）")
-
-
-@bot.command()
-async def なでなで(ctx):
-    await ctx.send(f"{ctx.author.mention} ニャァ～（ゴロゴロ）")
         
 @bot.command()
 async def et(ctx):
     dice = random.randint(1,6)
     await ctx.send(f"{ctx.author.mention} **{dice}** => `{emotionTable[dice-1]}`")
 
-@bot.command()
-async def リガルは美少女(ctx):
-    await ctx.send(f"{ctx.author.mention} それは違うのニャ！")
 
 async def random_choice(random_list):
     choice_num = random.randint(0, len(random_list)-1)
@@ -101,12 +117,11 @@ async def choice(ctx, *, arg):
         choice_text = choice_text[:comment_exist.start()]
     
     comma_exist = re.search(",", choice_text)
+
+    bracket_exist = re.search("\[(.*?)\]", choice_text)
+    if bracket_exist:
+        choice_text = bracket_exist.group(1)
     
-    
-    if choice_text[0] == "[":
-        choice_text = choice_text[1:]
-    if choice_text[-1] == "]":
-        choice_text = choice_text[:-1]
     if choice_text == "":
         await ctx.send("何か言ってほしいのニャ！")
         return -1
@@ -121,5 +136,10 @@ async def choice(ctx, *, arg):
     if comment_exist:
         markup_text = markup_text + f" `#{comment_word}`"
     await ctx.send(markup_text)
+    
+@bot.command()
+async def command(ctx):
+    await ctx.send(help_text)
 
+initialDate()
 bot.run(token)
